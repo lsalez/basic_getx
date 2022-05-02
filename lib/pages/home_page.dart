540ee225d445b61987c988328a91f61998beb059
ZBook <git@salez.mx>
2022-05-02 12:20:34 -0500

    UI finished

---
+++ lib/pages/home_page.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+import '../widgets/floating_action_buttons.dart';
+import '../widgets/my_text.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Basic GetX example'),
+      ),
+      body: const Center(
+        child: MyText(),
+      ),
+      floatingActionButton: const FloatingActionButtons(),
+    );
+  }
+}
